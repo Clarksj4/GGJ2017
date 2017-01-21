f7ac6e6ae465a07c77b867d176801203b2dc5fca
StephGarland <garlasl1@student.op.ac.nz>
2017-01-21 20:22:30 +1300

    Timer stuff

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float timeLeft = 120.0f;
+    public Text timeDisplay;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeDisplay.text = timeLeft.ToString("0.00");
+        if (timeLeft < 0)
+        {
+
+        }
+    }
+}
